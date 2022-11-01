1a2d19f0741e1e3230e0b83d2bf35a3fa0710f49
AlejandroMigliaccio <102888662+AlejandroMigliaccio@users.noreply.github.com>
2022-10-31 22:29:23 -0300

    Base

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,22 @@
 const logo = document.getElementById('insta');
+const card1 = document.getElementById('artTae');
+const card2 = document.getElementById('artKwon');
+const card3 = document.getElementById('artDo');
 
-logo.addEventListener('click', ()=>{
+logo.addEventListener('click', () => {
     window.open("https://www.instagram.com/ptg.training/",)
 })
 
+card1.addEventListener('click', () => {
+    window.location.replace('pages/taekwondo.html');
+})
 
+
+card2.addEventListener('click',()=> {
+    window.location.replace('pages/sucursales.html');
+})
+
+
+card3.addEventListener('click',()=> {
+    window.location.replace('pages/horarios.html');
+})
